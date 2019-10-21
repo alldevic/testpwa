b784f42466491751c3cfdedce4dddeb354e0aa88
DOXOPOKC <lilondin@gmail.com>
2019-10-21 00:20:35 -0400

    deploy

---
+++ precache-manifest.4d7c3c0a4e3f85085608fb141ef4686f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55b48b0dc667717dbed1",
+    "url": "/testpwa/css/app.0920f8d7.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/testpwa/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "8fb9ed3edb4dc9085dba98bc6c174dc6",
+    "url": "/testpwa/index.html"
+  },
+  {
+    "revision": "a280d3ab3fda711047a0",
+    "url": "/testpwa/js/about.4eb54e8e.js"
+  },
+  {
+    "revision": "55b48b0dc667717dbed1",
+    "url": "/testpwa/js/app.8075c4a3.js"
+  },
+  {
+    "revision": "75c3e5cff229556299c1",
+    "url": "/testpwa/js/chunk-vendors.68958384.js"
+  },
+  {
+    "revision": "4d10180f8c54b0be19d69a08b7beb286",
+    "url": "/testpwa/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/testpwa/robots.txt"
+  }
+]);
